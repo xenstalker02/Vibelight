8c6937297aa4fcccc062c59faebcd1c50c9cadea
Cameron Gutman <aicommander@gmail.com>
2026-05-16 16:09:57 -0500

    Fix incorrect apply() return value for legacy DRM clients
    (cherry picked from commit b33a252739a967db093d9d09e772b8b598523556)

--- app/streaming/video/ffmpeg-renderers/drm.h
+++ app/streaming/video/ffmpeg-renderers/drm.h
@@ -530,7 +530,7 @@ class DrmRenderer : public IFFmpegRenderer {
 
         bool apply() {
             if (!m_Atomic) {
-                return 0;
+                return true;
             }
 
             drmModeAtomicReqPtr req = nullptr;
